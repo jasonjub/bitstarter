cf199fb67545a3bf2471f88992d04bd5c91015c1
Jason Jubinville <jjubinvill@gmail.com>
2013-07-10 23:33:31 +0000

    changed 'index'

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buf = new Buffer(256);
-buf = fs.readFile(index.html);
+buf = fs.readFileSync('index.html');
 var string1 = buf.toString();
 
 
