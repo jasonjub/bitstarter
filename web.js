75f3b665d5f855f71dc0de210130231b34f24975
Jason Jubinville <jjubinvill@gmail.com>
2013-07-10 23:38:28 +0000

    Add new lines in function

--- web.js
+++ web.js
@@ -2,14 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(256);
-buf = fs.readFileSync('index.html');
-var string1 = buf.toString();
+
+
+
+
+
 
 
 
 app.get('/', function(request, response) {
-  response.send(string1);
+    var buf = new Buffer(256);
+    buf = fs.readFileSync('index.html');
+    var string1 = buf.toString();
+    response.send(string1);
 });
 
 var port = process.env.PORT || 5000;
